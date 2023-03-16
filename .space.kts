1a61489dcb3ea82ee143dc3e23c5d50a4696f860
Kasper De Vries <kasper.j.s.de.vries@gmail.com>
2023-03-16 19:12:59 +0000

    set release flag

--- .space.kts
+++ .space.kts
@@ -2,15 +2,18 @@ job("CMake Build & Test") {
   startOn {
     gitPush { enabled = true }
     }
+  parameters {
+    text("build-type", value = "Release")
+    }
     container(displayName = "CMake", image = "ubuntu")
     {
       shellScript {
         content = """
         apt update
         apt install cmake -y
-        cmake -B build --Release
-        cmake --build build --Release
-        ctest --test-dir build/test
+        cmake -B build -DCMAKE_BUILD_TYPE={{ build-type }}
+        cmake --build build --config {{ build-type }}
+        ctest --test-dir build/test -C {{ build-type }}
         """
         }
       }
