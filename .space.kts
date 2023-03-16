79888301d4ffa6160dd5aa39e034584d7261be36
Kasper de Vries <kasper.j.s.de.vries@gmail.com>
2023-03-16 20:38:34 +0100

    Add caching to cmake ci + formatted it

--- .space.kts
+++ .space.kts
@@ -1,20 +1,22 @@
 job("CMake Build & Test") {
-  startOn {
-    gitPush { enabled = true }
+    startOn {
+        gitPush { enabled = true }
     }
-  parameters {
-    text("build-type", value = "Release")
+    parameters {
+        text("build-type", value = "Release")
     }
-    container(displayName = "CMake", image = "ubuntu")
-    {
-      shellScript {
-        content = """
-        apt update
-        apt install build-essential cmake -y
-        cmake -B build -DCMAKE_BUILD_TYPE={{ build-type }}
-        cmake --build build --config {{ build-type }}
-        ctest --test-dir build/test -C {{ build-type }}
+    container(displayName = "CMake", image = "ubuntu") {
+        cache {
+            localPath = "build/*"
+        }
+        shellScript {
+            content = """
+                apt update
+                apt install build-essential cmake -y
+                cmake -B build -DCMAKE_BUILD_TYPE={{ build-type }}
+                cmake --build build --config {{ build-type }}
+                ctest --test-dir build/test -C {{ build-type }}
         """
         }
-      }
-  }
+    }
+}
