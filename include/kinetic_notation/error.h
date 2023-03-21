f19ccdbe21540889d030667154450956cc8ac1e7
Kasper de Vries <89792927+KasperJSdeVries@users.noreply.github.com>
2023-03-21 05:23:44 +0100

    Refactor error to look slightly nicer

--- include/kinetic_notation/error.h
+++ include/kinetic_notation/error.h
@@ -1,10 +1,6 @@
 #ifndef KINETIC_NOTATION_KINETIC_NOTATION_ERROR_H
 #define KINETIC_NOTATION_KINETIC_NOTATION_ERROR_H
 
-typedef struct {
-  char *message;
-} KnError;
-
 const char *kinetic_notation_get_error();
 
 #endif // KINETIC_NOTATION_KINETIC_NOTATION_ERROR_H
